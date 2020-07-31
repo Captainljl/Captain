a6f7a7435e5c60034d88dbfec21e110a7ce98427
liujialei <liujialei@xiaomi.com>
2020-07-31 21:31:19 +0800

    delete some message !

--- src/main/java/com/ljl/test/Test.java
+++ src/main/java/com/ljl/test/Test.java
@@ -3,8 +3,7 @@ package com.ljl.test;
 public class Test {
 
     public static void main(String[] args) {
-        BuildStage stage = BuildStage.parseByKey("START");
-        System.out.println(BuildStage.START.equals(stage));
+        System.out.println("hello world !");
     }
 
 }
